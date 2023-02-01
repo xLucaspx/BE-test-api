5a99796acc7630e5fb18a25f84c548ab3473d1aa
Lucas da Paz <xlucaspx@gmail.com>
2023-02-01 19:02:27 -0300

    Add product routes: get and getById

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const users = require("./userRoutes");
+const products = require("./productRoutes");
 
 module.exports = (app) => {
-  app.use(bodyParser.json(), users);
+  app.use(bodyParser.json(), users, products);
 };
