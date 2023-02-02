9faab6a5806e2872295833fef2d56ab464a615d8
Lucas da Paz <xlucaspx@gmail.com>
2023-02-02 20:29:10 -0300

    Refactor: using json-server to store data
    Significant performance improvement compared to the address generated with mockend; stored the data generated by mockend in db.json

--- api/services/ProductServices.js
+++ api/services/ProductServices.js
@@ -2,7 +2,26 @@ const Services = require("./Services");
 
 class ProductServices extends Services {
   constructor() {
-    super("https://mockend.com/xLucaspx/BE-test-api/products");
+    // super("https://mockend.com/xLucaspx/BE-test-api/products");
+    super("http://localhost:3000/products");
+  }
+
+  async getProductPrice(id) {
+    const product = await this.getOneRecord(id);
+    return product.price;
+  }
+
+  // ids must be a comma separated list (id1,id2,id3,...)
+  async getListOfPrices(ids) {
+    const productIds = ids.split(",");
+    const prices = [];
+
+    await productIds.forEach(async (id) => {
+      const price = await this.getProductPrice(id);
+      prices.push(Number(price));
+    });
+
+    return prices;
   }
 }
 
--- api/services/UserServices.js
+++ api/services/UserServices.js
@@ -2,7 +2,13 @@ const Services = require("./Services");
 
 class UserServices extends Services {
   constructor() {
-    super("https://mockend.com/xLucaspx/BE-test-api/users");
+    // super("https://mockend.com/xLucaspx/BE-test-api/users");
+    super("http://localhost:3000/users");
+  }
+
+  async getTaxValue(id) {
+  const user = await this.getOneRecord(id);
+  return user.tax / 100;
   }
 }
 
